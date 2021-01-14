1162f8a67b938339417e9ae89ea6f3d1329c4831
emotionalproof <chaserabenn@gmail.com>
2021-01-14 11:50:22 -0500

    Done.

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,32 @@
-# code here!
+require 'pry'
+
+class School
+    attr_accessor :roster
+
+    def initialize(name)
+        @name = name
+        @roster = {}
+    end
+
+
+    def add_student(student, grade)
+        if roster[grade]
+            roster[grade] << student
+        else
+            roster[grade] = [student]
+        end
+    end
+
+    def grade(grade)
+        roster[grade]
+    end
+
+    def sort
+        sorted = {}
+        roster.each do |k, v|
+            sorted[k] = v.sort
+        end
+        sorted
+    end
+
+end
